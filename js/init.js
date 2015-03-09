38749bfa6c3a1aed0491b4f963a324fe9e3a7c24
Ivan Smirnov <netlenskiy@yandex.ru>
2015-03-09 22:05:39 +0300

    Begun reworking all by modules. Added "mediator", reworked "menu" and "init".

--- js/init.js
+++ js/init.js
@@ -1,8 +1,7 @@
 //2
-var myMap, objects, objectsCopy;
-function init() { // Отвечает за загрузку карты, геолокацию
-
-	myMap = new ymaps.Map('wsMap', {
+function initGeoObjectManager() { // Отвечает за загрузку карты, геолокацию
+	var self = this;
+	var myMap = new ymaps.Map('wsMap', {
 		center: [55.20, 37.20],
 		zoom: 10
 	});
@@ -11,13 +10,12 @@ function init() { // Отвечает за загрузку карты, геол
 		'<figure>' +
 	    	'<img src="{{properties.imageAddress}}" ' +
 	    	' style="width:150px; height:150px;" ' +
-	    	' onclick="showGallery({{properties.coordinates}})"/>' +
+//запрашивается адрес картинок для метки либо по координатам либо по id
+	    	' onclick="mediator.requestImageSrc({{properties.coordinates}})"/>' +
 	    	'<figcaption>{{properties.disclamer}}</figcaption>' +
 	    '</figure>' +
 	    '</div>'
 		);
-
-
 
 	//ymaps.layout.storage.add('my#Layout', myBalloonLayout); - Why it does not work?
 	
@@ -44,76 +42,19 @@ function init() { // Отвечает за загрузку карты, геол
 	})
 	myMap.geoObjects.add( mainCollection );
 
-
-
 	objects = ymaps.geoQuery(myMap.geoObjects);
 	objectsCopy = ymaps.geoQuery(myMap.geoObjects);
 
-	function Navigation(elem, args) {
-		var self = this;
-		var menu = args;
-		var isOpen = false;
-		var lastOpened = "";
-
-		self.filterPlcmarks = function (target) {// ПЕРЕДЕЛАТЬ С ИСПОЛЬЗОВАНИЕМ ObjectManager
-			var temp = target.parentNode.getElementsByTagName("input");
-			var checked = [];
-			for (var i = 0; i < temp.length; i++) {
-				if ( temp[i].checked ) {
-					checked.push(i);
-				}
-			};
+	window.map = {
+		filterPlacemarks: function (types) {
 			mainCollection.removeAll();
 			for (var i = 0; i < items.length; i++) {
-				for (var j = 0; j < checked.length; j++) {
-					if ( items[i].properties._data.category == checked[j] )
+				for (var j = 0; j < types.length; j++) {
+					if ( items[i].properties._data.category == types[j] )
 						break;
 				}
-				if (j == checked.length) mainCollection.add( items[i] );
+				if (j == types.length) mainCollection.add( items[i] );
 			};
 		}
-		////////////////////////////////////////////////////////////////////
-		self.showHide = function (arr, clssName) {
-			for (var i = 0; i < arr.length; i++)
-					arr[i].className = clssName;
-		}
-		self.open = function (target) {
-			var li = target.getElementsByTagName("li");
-			if (!isOpen) {
-				self.showHide(li, "blockMenu");
-				isOpen = !isOpen;
-			} else  if (target == lastOpened) {
-				self.showHide(li, "noneMenu");
-				isOpen = !isOpen;
-			} else if (target.id == "filter"){
-				self.showHide( lastOpened.getElementsByTagName("li"), "noneMenu" );
-				self.filterPlcmarks(target);
-				isOpen = !isOpen;
-			} else {
-				self.showHide( lastOpened.getElementsByTagName("li"), "noneMenu" );
-				self.showHide( li, "blockMenu");
-				lastOpened = target;
-			}
-		}
-
-		function onclckHandler(e) {
-			var event = e || window.event;
-			var target = event.target;
-			if (target.id) {
-				self.open(target);
-				lastOpened = target;
-			}
-		}
-
-		elem.addEventListener("click", onclckHandler, false);
-	};
-
-
-	var menuItems = {
-		categories:  new Filter( document.getElementById("mainMenu").getElementsByTagName("ul")[0] )
-		, actions:  new Menu( document.getElementById("mainMenu").getElementsByTagName("ul")[1] )
-		, about:  new Menu( document.getElementById("mainMenu").getElementsByTagName("ul")[2] )
-		, search:  new Menu( document.getElementById("mainMenu").getElementsByTagName("ul")[3] )
-	};
-	var nav = new Navigation(mainMenu, menuItems);
+	}
 }
--- js/script.js
+++ js/script.js
@@ -1,30 +1,4 @@
 //5
-var myMapGeoObjects = ymaps.ready(init);
-// var filter = new Filter( document.getElementById("mainMenu").getElementsByTagName("ul")[0] );
-// var actions = new Menu( document.getElementById("mainMenu").getElementsByTagName("ul")[1] );
-// var about = new Menu( document.getElementById("mainMenu").getElementsByTagName("ul")[2] );
-// var search = new Menu( document.getElementById("mainMenu").getElementsByTagName("ul")[3] );
-
-
-console.log(objects);
-
-// var mpm = {
-// 		"id": 9,
-// 		"coordnts": [55.15, 37.15],
-// 		"srcString": "face/Smolensk_skver_pamyati_geroyev.jpg",
-// 		"disclamer": "Sed pretium euismod erat ut congue. Maecenas ac maximus enim, ac bibendum turpis. Aliquam erat volutpat.",
-// 		"icontype": 0,
-// 		"icon": "RedStarPic.gif"
-// 	};
-// 	console.log(myMapGeoObjects);
-// 	myMapGeoObjects.add(
-// 			new ymaps.Placemark( mpm.coordnts, {
-// 				imageAddress: mpm.srcString,
-// 				coordinates: mpm.coordnts,
-// 				disclamer: mpm.disclamer,
-// 				category: mpm.icontype,
-// 			}, {
-// 				//balloonContentLayout: myBalloonLayout,//'my#Layout' - Why it does not work?
-// 				iconLayout: "default#image",
-// 				iconImageHref: "icons/" + mpm.icon
-// 			} ) );
+ymaps.ready(initGeoObjectManager);
+var menu = new MenuCategory( $("#categories") );
+var mediator = new Mediator();
