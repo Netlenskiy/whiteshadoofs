c7730d3821fe9fd7951687d4761f1ceab2cc71af
Ivan Smirnov <netlenskiy@yandex.ru>
2015-03-27 21:02:10 +0300

    gallery reworked, animation effects and scrolling added.

--- js/gallery.js
+++ js/gallery.js
@@ -1,102 +1,84 @@
 //6
-	// загружаем фотки, метка идентифицируется по координатам
-	//считаем количество фоток, вычисляем размер блока
-	// создаем элементы в DOM
-	// обеспечиваем анимацию в случае необходимости
-	// по клику на балун запращивается адрес папки с фотками для данной метки
-	// и адреса фоток загружаются в массив
-
 function Gallery() {
 	var self = this;
 	var isOpened = false;
-	var aside = $( "#aside");
 	var section = $("#section");
-	var gallery = $( document.createElement("div") );
-	var opts = {
-		delay: 50,
-		duration: 1000,
-		step: function () {
-			var h = document.documentElement.clientHeight;
-			var needScroll = 1050 - h - document.body.scrollTop;
-			document.body.scrollTop += 5;
-		}
-	};
-	var options= {
-		delay: 20,
-		duration: 100,
-		count: 0,
-		step: function() {
-			//gallery.style.height = h * progress + "px";
-			aside.style.height = 850 - (this.count += 50) + "px";
-			console.log(" aside.style.height = " + aside.style.height);
-		}
-	};
-
-	gallery.attr( "id", "gallery" );
-	gallery.toggleClass( "openGallery" );
-
+	var canselButton = $( document.createElement("div") )
+	.addClass('canselButton')
+	.on("click", function () {
+		hideGallery();
+	});
+	var wrapper = $( document.createElement("div") )
+	.css({
+		position: "relative"
+	})
+	.append( canselButton );
+	var gallery = $( document.createElement("div") )
+	.attr( "id", "gallery" )
+	.toggleClass( "openGallery" )
+	.append( wrapper );
+	$.fx.speeds["very_slow"] = 1000;
+	
 	self.open = function (images) {
-		console.log("gallery.open()", " isOpened = ", isOpened);
-		var imgs = [];
-		if (!isOpened) {
-			for (var i=0; i<images.length; i++) {
-				imgs.push( $( document.createElement("img") ) );
-				imgs[i].attr( "src", images[i] );
-				gallery.append( imgs[i] );
-				console.log("image added");
-			}
-			var wrapper = $( document.createElement("div") );
-			var canselButton = $( document.createElement("div") );
-			canselButton.addClass('canselButton');
-			canselButton.on("click", function () {
-				hideGallery(options);
-			});
-			wrapper.append( canselButton );
-			wrapper.css( "position", "relative" );
-			section.append( wrapper );
-			section.append( gallery );
-			var timer = setInterval( function() {
-				var hgth = document.body.scrollTop;
-				var ns = 1050 - document.documentElement.clientHeight - hgth;
-				console.log("timer ", hgth);
-				if (hgth < ns)
-					opts.step();
-				else clearInterval(timer);
-			}, opts.delay );
+		if (isOpened) {
+			$("#gallery>img").remove();
+			appendImages(images);
+		} else {
+			appendImages(images);
+			show();
+			isOpened = !isOpened;
 		}
 	}
 
-	function animate(opts) {	
-		var h = 250;
-		var start = new Date();
-		var timer = setInterval( function () {
-			var progress = (new Date() - start)/opts.duration;
-			if (progress < 1)
-				opts.step(progress, h);
-			else
-				clearInterval(timer);
-		}, opts.delay );
+	function appendImages(images) {
+		var imgs = [];
+		for (var i=0; i<images.length; i++) {
+			imgs.push( $( document.createElement("img") )
+				.attr({
+					src: images[i],
+					width: 200,
+					height: 200
+				})
+				.css({
+					margin: 5,
+					border: "1px solid grey"
+				})
+			);
+			gallery.append( imgs[i] );
+		};		
 	}
-	function hideGallery (options) {	
-			var h = 250;
-			var start = new Date();
-			var timer = setInterval( function () {
-				var progress = (new Date() - start)/options.duration;
-				options.step();
-				if (progress > 1) 
-					clearInterval(timer);
-			}, options.delay );
-			section.removeChild( wrapper );
-			section.removeChild( gallery );
+	function windowScroll(into) {
+		var html = document.documentElement;
+		var body = document.body;
+		var elem = html.scrollTop ? html : body; 
+		var s_h = into - document.documentElement.clientHeight;
+		$(document.body).animate({
+			scrollTop: s_h
+		}, {
+			duration: "very_slow"
+		});
 	}
-	var opts = {
-		delay: 20,
-		duration: 500,
-		step: function(progress, h) {
-			gallery.css( "height", h * progress + "px" );
-			aside.css( "height", 600 + h * progress + "px" );
-			// gallery.scrollIntoView(false);	
-		}
-	};
-	//animate(opts);
+	function show() {
+		section.append( gallery );
+		windowScroll(780);
+		gallery.animate({
+			height: 250,
+			top: 350
+		}, {
+			duration: "very_slow"
+		});
+	}
+	function hideGallery (options) {
+		isOpened = !isOpened;
+		gallery.animate({
+			height: 0,
+			top: 600
+		}, {
+			duration: "very_slow",
+			queue: true
+		});
+		setTimeout(function () {
+			$("#gallery>img").remove();
+		}, 1000);
+	}
 }
