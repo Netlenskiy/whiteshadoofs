9d78fca2691c9e53dbb95df1657495667579e924
Ivan Smirnov <netlenskiy@yandex.ru>
2015-04-03 21:17:40 +0300

    Gallery was completed.

--- js/gallery.js
+++ js/gallery.js
@@ -1,27 +1,28 @@
 //6
-function Gallery() {
+
+function Gallery(gallery) {
+	var imgWidth = 200;
+	var posLeft = 5;
+	var gap = 5;
 	var self = this;
 	var isOpened = false;
 	var section = $("#section");
-	var canselButton = $( document.createElement("div") )
-	.addClass('canselButton')
-	.on("click", function () {
+	var carousel = $("#carousel");
+	var carouselWidth = parseInt( $("#carousel").css("width"), 10 );
+	$.fx.speeds["very_slow"] = 1000;
+	
+	$(".canselButton", gallery).on("click", function () {
 		hideGallery();
 	});
-	var wrapper = $( document.createElement("div") )
-	.css({
-		position: "relative"
-	})
-	.append( canselButton );
-	var gallery = $( document.createElement("div") )
-	.attr( "id", "gallery" )
-	.toggleClass( "openGallery" )
-	.append( wrapper );
-	$.fx.speeds["very_slow"] = 1000;
+	$(".scrollButton", gallery).on('click', function(event) {
+		event.preventDefault();
+		scrollImages( $("img", carousel), $(event.target).attr("id") );
+	});
+	gallery.toggleClass( "openGallery" );
 	
 	self.open = function (images) {
 		if (isOpened) {
-			$("#gallery>img").remove();
+			$("#img", carousel).remove();
 			appendImages(images);
 		} else {
 			appendImages(images);
@@ -29,41 +30,38 @@ function Gallery() {
 			isOpened = !isOpened;
 		}
 	}
-
 	function appendImages(images) {
 		var imgs = [];
 		for (var i=0; i<images.length; i++) {
-			imgs.push( $( document.createElement("img") )
-				.attr({
-					src: images[i],
-					width: 200,
-					height: 200
+			imgs.push( $( "<img/>", {
+					src: images[i]
+				} ).css({
+					left: posLeft,
+					outline: "1px solid grey"
 				})
-				.css({
-					margin: 5,
-					border: "1px solid grey"
-				})
+				.attr("pos", posLeft)
 			);
-			gallery.append( imgs[i] );
-		};		
+			posLeft += (imgWidth + gap);
+			carousel.append( imgs[i] );
+		};
 	}
-	function windowScroll(into) {
+	function scrollWindow(into) {
 		var html = document.documentElement;
 		var body = document.body;
 		var elem = html.scrollTop ? html : body; 
-		var s_h = into - document.documentElement.clientHeight;
+		var scrollHeight = into - document.documentElement.clientHeight;
 		$(document.body).animate({
-			scrollTop: s_h
+			scrollTop: scrollHeight
 		}, {
 			duration: "very_slow"
 		});
 	}
 	function show() {
 		section.append( gallery );
-		windowScroll(780);
+		scrollWindow(780);
 		gallery.animate({
-			height: 250,
-			top: 350
+			height: 230,
+			top: 370
 		}, {
 			duration: "very_slow"
 		});
@@ -75,10 +73,33 @@ function Gallery() {
 			top: 600
 		}, {
 			duration: "very_slow",
-			queue: true
+			complete: function () {
+				$("img", carousel).remove();
+			} 
 		});
-		setTimeout(function () {
-			$("#gallery>img").remove();
-		}, 1000);
+	}
+	function scrollImages(images, into) {
+		var firstPos = parseInt( $( images[0] ).attr("pos"), 10 );
+		var lastPos = parseInt( $( images[images.length-1] ).attr("pos"), 10 );
+		var shift = imgWidth + gap;
+		if (into == "left")
+		{
+			if (lastPos <= carouselWidth - imgWidth) return;
+			shift = -shift;
+		}
+		else
+		{
+			if (firstPos >= gap) return;
+		}
+		images.each(function(index, el) {
+			var e = $(el);
+			var newPos = shift + parseInt( e.attr("pos"), 10 );
+			e.animate({
+				left: newPos
+			}, {
+				duration: "slow"
+			});
+			e.attr("pos", newPos);
+		});
 	}
 }
--- js/script.js
+++ js/script.js
@@ -2,4 +2,5 @@
 ymaps.ready(initGeoObjectManager);
 var menu = new MenuCategory( $("#categories") );
 var mediator = new Mediator();
-var gallery = new Gallery();
+var gallery = new Gallery( $("#gallery") );
+//mediator.requestImageSrc(1);
