1520b9467d78b80801f7203cbac032600a6f7f05
Netlenskiy <netlenskiy@yandex.ru>
2015-08-23 18:07:31 +0300

    Deleted static files from root directory after pull from ws with old code.

--- js/script.js
+++
@@ -1,6 +0,0 @@
-//5
-ymaps.ready(initGeoObjectManager);
-var menu = new Menu( $("#mainMenu") );
-var mediator = new Mediator();
-var gallery = new Gallery( $("#gallery") );
-//mediator.requestImageSrc(1);
